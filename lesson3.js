07d8053bbcdfb9f1cab2c73572fe025092543475
Yurii.Kravchenko <yurij772@gmail.com>
2026-03-11 14:04:56 +0200

    commit due to los data

--- lesson3.js
+++ lesson3.js
@@ -6,3 +6,13 @@ for (let i = 0; i <= power; i++) {
 return f;
 }
 console.log(pow (3, 4));
+
+
+
+function isEven(x) {
+  if (x % 2 === 0) { return "true" }
+    else {return flase}
+}
+
+const number = 7;
+console.log(`Число ${number} парне: ${isEven(number)}`);
