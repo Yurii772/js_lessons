9b825f812ea8a0c5e4ee22a9c9bc820bc4a0238c
Yurii.Kravchenko <yurij772@gmail.com>
2026-03-16 12:01:43 +0200

    All required outputs added

--- lesson4.js
+++ lesson4.js
@@ -1,4 +1,3 @@
-
 let countEven = 0;
 let countOdd = 0;
 let checkProbabilityTheory = function (count){  
@@ -6,17 +5,18 @@ for (i = 1; i<=count; i++){
    let number = Math.floor(Math.random() * (1000 - 100 + 1)) + 100;
 if (number%2 === 0){
 countEven++;
-   console.log(number);   
 }
 else {
 countOdd++;
-console.log (number); 
 }
 }
    let evenProbability = countEven / count;
    let oddProbability = countOdd / count;
-   console.log("Even Probability=".concat(evenProbability * 100).concat("%"));
-   console.log("Odd Probability=".concat(oddProbability * 100).concat("%"));
+   console.log ("Total nubers generated ".concat (count));
+   console.log ("Even count ".concat (countEven));
+   console.log ("Odd count ".concat (countOdd));
+   console.log("Even Probability = ".concat(evenProbability * 100).concat("%"));
+   console.log("Odd Probability = ".concat(oddProbability * 100).concat("%"));
 }
-checkProbabilityTheory(9);
+checkProbabilityTheory(8);
 
