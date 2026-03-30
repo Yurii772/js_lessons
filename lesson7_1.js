57617413ae3836e6febbb70448b3ebf5a80f32df
Yurii.Kravchenko <yurij772@gmail.com>
2026-03-30 10:43:21 +0300

    test data

---
+++ lesson7_1.js
@@ -0,0 +1,18 @@
+var arr = [
+   {
+      userName: "Test",
+      lastName: "Test",
+      email: "test.test@gmail.com"
+   },
+   {
+      userName: "Dmitro",
+      lastName: "Porohov",
+        < email: "dmitro.porohov@yahoo.com>"
+    },
+{
+   userName: "Andrii",
+      lastName: "",
+         email: "andrii@mail.ru" // Нам такі не підходять
+},
+];
+let 
---
+++ lesson7_2.js
@@ -0,0 +1,1 @@
+let str = 'Happiness, Time, Task, Apple, Wonderful, Joyful'
