7b6704ff89ae0e79db265968f47a74479015185b
Yurii.Kravchenko <yurij772@gmail.com>
2026-03-30 15:19:19 +0300

    task 1 completed

--- lesson7_1.js
+++ lesson7_1.js
@@ -13,14 +13,35 @@ var arr = [
    {
       userName: "Test",
       lastName: "Test",
-      email: "testru.test@gmail.com" 
+      email: "test-ru.test@gmail.com" 
 },
+   {
+      userName: "Test",
+      lastName: "Test",
+      email: "opanas-ru.test@gmail.com"
+   },
+   {
+      userName: "Test",
+      lastName: "Test",
+      email: "Boris.theBlade@mail.ru"
+   },
+   {
+      userName: "Test",
+      lastName: "Test",
+      email: "I'm_notvail%$#.test@gmail.com"
+   }
 ];
 let filteredArray = arr.filter(el => el.email);
 let emailsOnly = filteredArray.map(filteredArray => filteredArray.email);
-let russianDetection = /ru/g;
+let russianDetection = /ru$/g;
+console.log(emailsOnly);
 let approvedEmails = emailsOnly.filter(email => {
    return !russianDetection.test(email);
-})
-console.log(approvedEmails);
+});
+let emailValidation = /[\w\.-]/g;
+console.log("We can prove this users: ".concat(approvedEmails));
+let validAmoungApproved = approvedEmails.filter(email =>{
+   return !emailValidation(email);
+});
+console.log(validAmoungApproved);
 
